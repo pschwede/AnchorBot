8ba4168d56db24d790f9713fc207e22fe5ea4da2
spz <Peter.Schwede@gmx.net>
2012-05-09 12:53:33 +0200

    fix: pip instead of distro-packages

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -3,7 +3,7 @@
 # Please read install scripts before running them!
 
 # this will install all needed python modules and libraries to build lxml
-apt-get install python-feedparser python-sqlite python-setuptools libxml2-dev libxslt1-dev python2.7-dev hyphen-de &&
+apt-get install libxml2-dev libxslt1-dev python2.7-dev &&
 
 # this will download newest packages and install them
-easy_install tweepy beautifulsoup lxml sqlalchemy
+pip install feedparser sqlite setuptools tweepy beautifulsoup lxml sqlalchemy
