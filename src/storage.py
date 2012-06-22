5d5e6415c732e9d7e6ef7f12f6b03bd916bbaac0
spz <Peter.Schwede@gmx.net>
2012-06-22 16:28:12 +0200

    fix: couldn't print non-ascii character -- f***ing bad utf support

--- src/storage.py
+++ src/storage.py
@@ -110,7 +110,7 @@ class FileCacher(dict):
                 self.dloader.retrieve(url, newurl)
                 done = True
             except Exception, e:
-                log("IOError during retrieving %s: %s" % (url, str(e)))
+                log("IOError during retrieving: %s" % (str(e)))
 
     def __queued_retrieve(self):
         while self.running:
