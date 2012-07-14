300b33082b667435ed319b113947bce29ab7b330
spz <Peter.Schwede@gmx.net>
2012-07-14 17:45:37 +0200

    forks after popping up webbrowser

--- web.sh
+++ web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Opening it in browser"
-x-www-browser http://0.0.0.0:8000
+x-www-browser http://0.0.0.0:8000 &
 echo "Starting web interface"
 python $(dirname "$0")/src/server.py $1 $2 $3
